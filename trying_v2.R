055c756989e8379e50432f4f534638754c64beef
MAValle <mauvalle@gmail.com>
2019-04-24 14:17:10 -0400

    I upgraded folder 2404191317

--- trying_v2.R
+++ trying_v2.R
@@ -22,8 +22,8 @@ data <- data[complete.cases(data), ]
 
 #input:
 #columnas en data en las que se encuentra cada tipo de instrumento
-columnas <- list("america" = c(2:9), "europe" = c(10:18), "asia" = c(19:25),
-              "oceania" = c(29:30), "commodities" = c(31:34), "bonds" = c(26:28),
+columnas <- list("america" = c(2,6,7,8,9,10,11,12), "europe" = c(13,14,15,16,17,18,19,20,21), "asia" = c(22,23,24,25,26,27,30),
+              "oceania" = c(28,29), "commodities" = c(31,32,33,34), "bonds" = c(3,4,5),
               "all_indices" = c(2:34))
 #ejemplo: colsy$america
 # length(colsy) = 6
@@ -82,10 +82,72 @@ while ( fin <= nrow(data) ) {
 # # # ## ## ## ## ## ## ## # CODE # ## ## ## ## ## ## ## #
 # beauty of the final data before to send
 storage_data <- as.data.frame(storage_data)
-colnames(storage_data) <- c("bloack", names(columnas))
+colnames(storage_data) <- c("block", names(columnas))
 storage_data <- storage_data[complete.cases(storage_data), ]
 write.csv(storage_data,'mst_lengths_daily_170419.csv')
 write.csv(storage_data,'mst_lengths_weekly_170419.csv')
 # # # ## ## ## ## ## ## ## # CODE # ## ## ## ## ## ## ## #
 
 plot(storage_data[,7], type="l" )
+
+
+
+# 24-abr-19
+# viendo como calcular el largo que hay entre isntrumentos de un mismo tipo 
+# dentro de un MST.
+# un plot de mst de un periodo cualquiera
+mstnet <- mst_g_list[[7]] # mst
+coords <- layout_nicely(mstnet, dim = 2)
+plot(mstnet, layout = coords,
+     vertex.size = 10,
+     edge.width = 3,
+     vertex.label.cex=0.6,
+     #vertex.color="white",
+     vertex.color="orange", 
+     vertex.frame.color="#ffffff",
+     vertex.label.color="black")
+
+names_america <- names(data[,c(2,6,7,8,9,10,11,12)]) # select the nodes having these names
+names_oceania <- names(data[,c(28,29)])
+
+# https://www.biostars.org/p/100850/
+#g2 <- induced.subgraph(graph=mstnet,vids=unlist(neighborhood(graph=mstnet,order=2,nodes=names_oceania))) # no sirve
+#https://ourednik.info/maps/2018/09/21/create-a-subgraph-from-the-neighborhood-of-specific-vertices-in-igraph/
+#selegoV <- ego(mstnet, order=1, nodes = names_america, mode = "all", mindist = 0) # me dan grafos desconectados
+#g2 <- subgraph(mstnet, names_america) # no sirve, deja los nodos aislados
+#g2 <- induced_subgraph(mstnet, vids=names(data[,c(2:9)])) #no sirve deja los nodos aislados
+#g2  <- induced_subgraph(mstnet,unlist(selegoV))
+plot(g2,
+     vertex.size = 10,
+     edge.width = 3,
+     vertex.label.cex=0.6,
+     #vertex.color="white",
+     vertex.color="orange", 
+     vertex.frame.color="#ffffff",
+     vertex.label.color="black")
+
+
+
+
+plot.igraph(g, layout = -coords[,2:1],
+            vertex.shape="rectangle",
+            vertex.size=10,
+            vertex.size2=1,
+            vertex.color=NA,
+            vertex.frame.color=NA,
+            vertex.label.color="black",
+            vertex.label.family="sans",
+            edge.label.color="white",
+            edge.arrow.mode=0,
+            edge.width=3,
+            asp=5)
+
+
+
+
+
+https://stackoverflow.com/questions/24597523/how-can-one-set-the-size-of-an-igraph-plot
+https://stackoverflow.com/questions/48746804/adjusting-visualization-of-igraph-plot
+https://www.r-graph-gallery.com/248-igraph-plotting-parameters/
+https://rstudio-pubs-static.s3.amazonaws.com/337696_c6b008e0766e46bebf1401bea67f7b10.html
+https://stackoverflow.com/questions/23367765/extract-a-connected-subgraph-from-a-subset-of-vertices-with-igraph
